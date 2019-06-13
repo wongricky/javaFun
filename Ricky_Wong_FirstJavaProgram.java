be24fe5eeb08ed3b44e06de446c1805b19a2a7ba
wongricky <50374597+wongricky@users.noreply.github.com>
2019-06-13 15:17:12 -0500

    Update Ricky_Wong_FirstJavaProgram.java

--- Ricky_Wong_FirstJavaProgram.java
+++ Ricky_Wong_FirstJavaProgram.java
@@ -1,4 +1,4 @@
-public class FirstJaveProgram{
+public class FirstJavaProgram{
     public static void main(String[] args){
         System.out.println("My name is Ricky");
         System.out.println("I am 30 years old");
