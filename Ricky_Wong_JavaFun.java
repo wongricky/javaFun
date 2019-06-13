711b492b38afe632878e1e09b87e7e6798afca3a
wongricky <50374597+wongricky@users.noreply.github.com>
2019-06-13 14:44:07 -0500

    Add files via upload

---
+++ Ricky_Wong_JavaFun.java
@@ -0,0 +1,7 @@
+public class PracticeProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Ricky");
+        System.out.println("I am 30 years old");
+        System.out.println("My hometown is Dallas, Texas");
+    }
+}
